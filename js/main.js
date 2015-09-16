c5878f890e846070d628adafa785841c5b2cdfa0
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2015-09-15 23:46:11 -0400

    Fuck

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ function redirect(src, dest) {
 
 redirect('/resume/', 'https://dl.dropboxusercontent.com/u/22196024/Resume%20-%20Kirby%20Kohlmorgen.pdf');
 redirect('/headshot/', 'http://www.gravatar.com/avatar/a153778db9dfc4a99055884515e8c336.jpg?s=500');
+
+$('#nav-brand').click(function() {
+  $('html, body').animate({
+      scrollTop: $(".about").offset().top
+    }, 700);
+  return false;
+});
