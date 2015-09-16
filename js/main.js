7c9d45006e04a3c9a50839cd5a0a648e54db9760
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2015-09-16 09:33:07 -0400

    Add basic arrow.

--- js/main.js
+++ js/main.js
@@ -7,9 +7,10 @@ function redirect(src, dest) {
 redirect('/resume/', 'https://dl.dropboxusercontent.com/u/22196024/Resume%20-%20Kirby%20Kohlmorgen.pdf');
 redirect('/headshot/', 'http://www.gravatar.com/avatar/a153778db9dfc4a99055884515e8c336.jpg?s=500');
 
-$('#nav-brand').click(function() {
+$('.arrow').click(function() {
+  console.log('clicked');
   $('html, body').animate({
-      scrollTop: $(".about").offset().top
+      scrollTop: $(".home-main").offset().top
     }, 700);
   return false;
 });
