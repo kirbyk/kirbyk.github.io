36fdc19b530651cd3d534dae6e8f329fcf0fe57e
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2015-09-16 11:39:07 -0400

    Much better css for positioning the arrow, and better animation killing

--- js/main.js
+++ js/main.js
@@ -11,10 +11,14 @@ $('.arrow').click(function() {
   $('html, body').animate({
       scrollTop: $(".home-main").offset().top
     }, 700);
+  clearInterval(bouncyArrow);
   return false;
+});
+
+$(window).scroll(function() {
   clearInterval(bouncyArrow);
 });
 
 var bouncyArrow = setInterval(function() {
   $('.arrow').toggleClass('animated bounce');
-}, 5000);
+}, 1000);
