64ad5b45dd2ab3a835e4da62b94904eeb1d0ce28
Kirby Kohlmorgen <kirbykohlmorgen@gmail.com>
2024-09-08 05:06:38 -0400

    more cleanup and replace twitter with linkedin

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,12 +1,3 @@
-function redirect(src, dest) {
-  if (window.location.pathname === src) {
-    window.location = dest;
-  }
-}
-
-redirect('/resume/', 'https://dl.dropboxusercontent.com/u/22196024/Resume%20-%20Kirby%20Kohlmorgen.pdf');
-redirect('/headshot/', 'http://www.gravatar.com/avatar/a153778db9dfc4a99055884515e8c336.jpg?s=500');
-
 $('.arrow').click(function() {
   $('html, body').animate({
       scrollTop: $(".home-main").offset().top
